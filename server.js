e6ad8d2247d6138fd4be533b2401917e155551f7
kumaarajesh <36688003+kumaarajesh@users.noreply.github.com>
2018-02-21 15:58:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article-one', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send('url is requested');
 });
-app.get('article-two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send(' 2 url is requested');
 });
 
